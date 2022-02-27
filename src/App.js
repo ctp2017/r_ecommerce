e77c24ed4397a33f9de95506b446440509674480
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-02-27 14:19:21 +0300

    المجلدات والملفات المطلوبة

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import'../src/assets/css/custom.css'
+export class App extends Component {
+  render() {
+    return (
+      <div>App1</div>
+    )
+  }
 }
 
-export default App;
+export default App
