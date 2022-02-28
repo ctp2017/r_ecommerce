5bb9fefb86ea8406dbca6578f2cd46973f103914
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-02-28 18:35:36 +0300

    remove

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -3,16 +3,14 @@ import"../assets/css/custom.css"
 import {Container,Navbar,Form,FormControl,Button} from'react-bootstrap'
 import Logo from'../assets/images/ctp.png'
 import"../assets/css/fontawesome.css"
-
 export class TopNavbar extends Component {
   render() {
     return (
     <Fragment>
-
-   
     <Navbar expand="lg" variant="light" className="navbar" >
+
       <Container fluid={true}>
-      
+
       <Navbar.Brand href="#home">
         <img
           alt=""
@@ -24,7 +22,6 @@ export class TopNavbar extends Component {
       Creative Team
       </Navbar.Brand>
 
-   
 
       <Form className="d-flex">
         <FormControl
@@ -36,31 +33,20 @@ export class TopNavbar extends Component {
         <a className="Search-btn"><i className="fa fa-search"></i></a>
       </Form>
 
+
+
       <a className="cart-btn"><i className="fa fa-shopping-cart"></i> items 0 </a>
-
-
       <a  herf=""><i className="fa h4 fa-heart"></i><sup><span className="badge text-white bg-danger">0</span></sup>  </a>
-
       <a herf=""><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">8</span></sup>  </a>
-    
-
       <a className='user-icon'  herf=""><i className="fa h4 fa-share" ></i> </a>
       <a  className='user-icon'  herf=""><i className="fa h4 fa-address-card" ></i> </a>
-     
-     
 
+
+      
       </Container>
     </Navbar>
-  
-
-
-
-
-
-
     </Fragment>
     )
   }
 }
-
 export default TopNavbar
