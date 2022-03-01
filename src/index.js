9211b15ed93fa46b9a1599a9c029d03b9d09df6c
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-03-01 21:49:30 +0300

    remove

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TopNavbar from './components/TopNavbar';
+import '../src/assets/css/custom.css';
+
 
 
 
