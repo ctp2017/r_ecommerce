6ba365c98b7e400ee7605228e481b141c67863dd
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-03-01 16:28:22 +0300

    الدرس الثاني - تصميم القائمة الجانبية

--- src/components/home/SideBarMenu.jsx
+++ src/components/home/SideBarMenu.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from 'react'
+import'../../assets/css/custom.css'
+
 
 export class SideBarMenu extends Component {
   render() {
     return (
-      <div>SideBarMenu</div>
+
+      <div className="accordionMenuDiv">
+                    <div className="accordionMenuDivInside">
+
+
+                         <button className="accordion">
+                              <img className="accordionMenuIcon" src="test.png" /> Living Room
+                         </button>
+                         <div className="panel">
+                              <ul>
+                                   <li><a href="#" className="accordionItem" > Sofa</a></li>
+                                   <li><a href="#" className="accordionItem" > Beds</a></li>
+                                   <li><a href="#" className="accordionItem" > Tv Units </a></li>
+                              </ul>
+                         </div>
+
+
+                         <button className="accordion">
+                         <img className="accordionMenuIcon" src="test.png" /> Living Room
+                    </button>
+                    <div className="panel">
+                         <ul>
+                              <li><a href="#" className="accordionItem" > Sofa</a></li>
+                              <li><a href="#" className="accordionItem" > Beds</a></li>
+                              <li><a href="#" className="accordionItem" > Tv Units </a></li>
+                         </ul>
+                    </div>
+
+
+                      
+
+                    </div>
+
+               </div>
+
+
+
+
+
     )
   }
 }
