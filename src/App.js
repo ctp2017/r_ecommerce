fc83f1e5685ee820d0a3924d996d28e254be1bf6
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-03-02 14:42:04 +0300

    الدرس الأول - إنشاء الملفات الخاصة بقسم الأصناف

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import'../src/assets/css/custom.css'
+
 import HomeTop from './components/home/HomeTop'
+import Categories from './components/home/Categories'
 import TopNavbar from './components/TopNavbar'
 export class App extends Component {
   render() {
@@ -9,6 +11,7 @@ export class App extends Component {
     <TopNavbar/>
 
     <HomeTop/>
+  <Categories/>
    
     
     </Fragment>
---
+++ src/components/home/Categories.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react'
+import {Container} from 'react-bootstrap'
+import'../../assets/css/custom.css'
+export class Categories extends Component {
+  render() {
+    return (
+     <Fragment>
+
+     <Container className="text-center" fluid={true}>
+     <div className="section-title text-center mb-55"><h2> CATEGORIES</h2>
+     <p>Some Of Our Exclusive Collection, You May Like</p>
+     </div>
+     </Container>
+     
+   
+     
+     </Fragment>
+    )
+  }
+}
+
+export default Categories
