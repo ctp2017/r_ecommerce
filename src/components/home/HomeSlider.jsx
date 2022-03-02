ab4aec35c942ad1c5f6d6c5775b7da2aaee13b1c
ctp2017 <100434017+ctp2017@users.noreply.github.com>
2022-03-02 11:17:16 +0300

    Slider Slick - الدرس الخامس - إضافة مكتبة عرض الشرائح

--- src/components/home/HomeSlider.jsx
+++ src/components/home/HomeSlider.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+import Slider from "react-slick";
+
 
 export class HomeSlider extends Component {
   render() {
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
+
+
     return (
-      <div>HomeSlider</div>
+      <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
     )
   }
 }
